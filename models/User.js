60fbcb6572113252bed1ebbcd180902b1dda130e
Mohamed6211 <88840019+Mohamed6211@users.noreply.github.com>
2026-03-27 17:13:24 +0100

    Update User.js

--- models/User.js
+++ models/User.js
@@ -1,13 +1,18 @@
 const mongoose = require("mongoose");
 
-const UserSchema = new mongoose.Schema({
-  email: { type: String, required: true, unique: true },
-  passwordHash: { type: String, required: true },
+const userSchema = new mongoose.Schema({
+  email: { type: String, unique: true },
+  passwordHash: String,
+
+  isVerified: { type: Boolean, default: false },
+  verificationCode: String,
+  verificationCodeExpires: Date,
+
   project: {
-    layersJSON: String,      // Leaflet layers as JSON
-    rasterPaths: [String],   // uploaded rasters
+    layersJSON: String,
+    rasterPaths: [String],
     lastEdited: Date
   }
 });
 
-module.exports = mongoose.model("User", UserSchema);
+module.exports = mongoose.model("User", userSchema);
